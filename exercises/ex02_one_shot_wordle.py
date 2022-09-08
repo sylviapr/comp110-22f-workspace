28aa0dba1483125e198312a3a0d9ea6d2a0b9c72
Sylvia Prevette <sylviapr@ad.unc.edu>
2022-09-07 21:34:26 -0400

    Commit at start of ex02_one_shot_wordle

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,6 @@
+"""An expansion of our previous exercise, Chardle!"""
+
+__author__ = "730575415"
+
+secret_word: str = "python"
+
--- lessons/function_def.py
+++ lessons/function_def.py
@@ -8,9 +8,6 @@ def my_max(a: int, b: int) -> int:
     else: 
         return b
 
-
-x:int = 6
-y: int = 5 + 2
-z: int = my_max(x, y)
-print(z)
-
+print(my_max(10 + 1, 10))
+result: int = my_max(-50, 100)
+print(result)
