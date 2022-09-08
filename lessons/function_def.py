bd46bad6de15727772e14279de1b7ce24f3a1990
Sylvia Prevette <sylviapr@ad.unc.edu>
2022-09-07 21:28:20 -0400

    Commit on 9/7 before starting EX02

--- lessons/function_def.py
+++ lessons/function_def.py
@@ -8,6 +8,9 @@ def my_max(a: int, b: int) -> int:
     else: 
         return b
 
-print(my_max(10 + 1, 10))
-result: int = my_max(-50, 100)
-print(result)
+
+x:int = 6
+y: int = 5 + 2
+z: int = my_max(x, y)
+print(z)
+
