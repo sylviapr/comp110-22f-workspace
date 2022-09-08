2f719d5c81579d6de7d9abfc5676704153427d20
Sylvia Prevette <sylviapr@ad.unc.edu>
2022-09-08 00:43:52 -0400

    Halfway through EX02!

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,17 +3,33 @@
 __author__ = "730575415"
 
 secret_word: str = "python"
+word_length: int = len(secret_word)
 
-guess: str = str(input("What is your 6-letter guess? "))
+guess: str = str(input(f"What is your {word_length}-letter guess? "))
 while len(guess) != len(secret_word):
-    guess = input("That was not 6 letters! Try again: ")
+    guess = input(f"That was not {word_length} letters! Try again: ")
 
 if guess == secret_word:
     print("Woo! You got it!")
 else: 
     print("Not quite! Play again soon! ")
-# Problem: "Additionally, be sure you are using 
-# f-String templates rather than concatenation 
-# for this output." How?
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+index_checking: int = 0
+emoji_result: str = ""
+
+while index_checking < len(secret_word):
+    if guess[index_checking] == secret_word[index_checking]:
+        emoji_result = emoji_result + GREEN_BOX
+    else: 
+        emoji_result = emoji_result + WHITE_BOX
+    index_checking = index_checking + 1
+
+print(emoji_result)
 
 
+
+#length of index checking? How to get loop going there
