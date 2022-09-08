518dd63fff0b050c7e17e70993173c10ebfd72aa
Sylvia Prevette <sylviapr@ad.unc.edu>
2022-09-08 00:16:29 -0400

    Commit after finishing (kind of) part 1 of EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,3 +4,16 @@ __author__ = "730575415"
 
 secret_word: str = "python"
 
+guess: str = str(input("What is your 6-letter guess? "))
+while len(guess) != len(secret_word):
+    guess = input("That was not 6 letters! Try again: ")
+
+if guess == secret_word:
+    print("Woo! You got it!")
+else: 
+    print("Not quite! Play again soon! ")
+# Problem: "Additionally, be sure you are using 
+# f-String templates rather than concatenation 
+# for this output." How?
+
+
