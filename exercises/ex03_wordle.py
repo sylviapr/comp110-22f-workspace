9c1156b7c4121ea3712c8f38d36246f68920ca3f
Sylvia Prevette <sylviapr@ad.unc.edu>
2022-09-12 20:12:38 -0400

    Continued EX03 and learned about lists

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,4 +4,5 @@ __author__ = "730575415"
 
 def contains_char(search_through: str, search_for: str) -> bool:
     """Returns True if the one-character second input is found anywhere in the first input, returns False otherwise."""
+    assert len(search_for) == 1
     
