db1df463db3a1da8f4f40fa0675e3e10ab28e8a6
Sylvia Prevette <sylviapr@ad.unc.edu>
2022-10-10 16:48:03 -0400

    Progress on EX07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -4,11 +4,37 @@ __author__ = "730575415"
 
 def invert(input: dict[str, str]) -> dict[str, str]:
     """A function that takes in a dictionary[str, str] and returns a new dictionary that has inverted the keys and values of the original."""
-    result: dict[str, str]
-    result = dict()
+    result: dict[str, str] = {}
     for key in input:
-        result = {[input[key]]: key}
+        if input[key] in result:
+            raise KeyError("Make sure that no values are repeated in the input dictionary.")
+        result[input[key]] = key
+    return result
 
 
-        # result[input[key]] == key
+def favorite_color(names: dict[str, str]) -> str:
+    """Given an input of a dict[str, str] consisting of names and favorite colors, returns a str that says the color that appears the most frequently in the input."""
+    colors: list[str] = []
+    counted_colors: dict[str, int] = {}
+    max: int = 0
+    max_color: str = ""
+    for name in names:
+        colors.append(names[name])
+    counted_colors = count(colors)
+    
+    for i in counted_colors:
+        if counted_colors[i] > max:
+            max = counted_colors[i]
+            max_color = i
+    return max_color
+    
+
+def count(letters: list[str]) -> dict[str, int]:
+    """Takes a list of strings and returns a dict that contains list values as keys and the number of times they were in the list as values."""
+    result: dict[str, int] = {}
+    for item in letters:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
     return result
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,5 +1,48 @@
 """Tests for our dictionary functions."""
 
+
 __author__ = "730575415"
 
-from exercises.ex07.dictionary import invert
+
+from exercises.ex07.dictionary import invert, favorite_color, count
+import pytest
+
+
+def test_invert_keyerror() -> None:
+    """Testing invert with a dictionary that contains a repeated value."""
+    # Edge case
+    with pytest.raises(KeyError):
+        my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(my_dictionary)
+
+
+def test_invert_short() -> None:
+    """Testing invert with a short regular dictionary."""
+    xs: dict[str, str] = {'cat': 'oreo', 'dog': 'addy'}
+    assert invert(xs) == {'oreo': 'cat', 'addy': 'dog'}
+
+
+def test_invert_long() -> None:
+    """Testing invert with a longer list."""
+    xs: dict[str, str] = {'cat': 'oreo', 'dog': 'addy', 'bird': 'mango', 'cow': 'bessie', 'lizard': 'rango', 'chicken': 'hen solo'}
+    assert invert(xs) == {'oreo': 'cat', 'addy': 'dog', 'mango': 'bird', 'bessie': 'cow', 'rango': 'lizard', 'hen solo': 'chicken'}
+
+
+
+
+
+
+def test_count_no_repeats() -> None:
+    """Testing count with a list that has no repeated strs."""
+    xs: list[int] = ["s", "y", "l", "v", "i", "a"]
+    assert count(["s", "y", "l", "v", "i", "a"]) == {'s': 1, 'y': 1, 'l': 1, 'v': 1, 'i': 1, 'a': 1}
+
+
+def test_count_repeats() -> None:
+    """Testing count with a list that does have repeated strs."""
+    assert count(["e", "s", "t", "e", "l", "l", "e"]) == {"e": 3, "s": 1, "t": 1, "l": 2}
+
+
+def test_count_numberstrs() -> None:
+    """Testing count with strings that consist of numbers."""
+    assert count(["1", "2", "3", "4", "5"]) == {"1": 1, "2": 1, "3": 1, "4": 1, "5": 1}
